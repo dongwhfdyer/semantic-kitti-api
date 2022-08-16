5f1691ee8d989c9d4425903011a8088fc2ffa7cb
kuhn <whfdyer@163.com>
2022-08-16 09:02:00 +0800

    first commit

--- auxiliary/SSCDataset.py
+++ auxiliary/SSCDataset.py
@@ -3,18 +3,18 @@ import numpy as np
 
 
 def unpack(compressed):
-  ''' given a bit encoded voxel grid, make a normal voxel grid out of it.  '''
-  uncompressed = np.zeros(compressed.shape[0] * 8, dtype=np.uint8)
-  uncompressed[::8] = compressed[:] >> 7 & 1
-  uncompressed[1::8] = compressed[:] >> 6 & 1
-  uncompressed[2::8] = compressed[:] >> 5 & 1
-  uncompressed[3::8] = compressed[:] >> 4 & 1
-  uncompressed[4::8] = compressed[:] >> 3 & 1
-  uncompressed[5::8] = compressed[:] >> 2 & 1
-  uncompressed[6::8] = compressed[:] >> 1 & 1
-  uncompressed[7::8] = compressed[:] & 1
+    ''' given a bit encoded voxel grid, make a normal voxel grid out of it.  '''
+    uncompressed = np.zeros(compressed.shape[0] * 8, dtype=np.uint8)
+    uncompressed[::8] = compressed[:] >> 7 & 1
+    uncompressed[1::8] = compressed[:] >> 6 & 1
+    uncompressed[2::8] = compressed[:] >> 5 & 1
+    uncompressed[3::8] = compressed[:] >> 4 & 1
+    uncompressed[4::8] = compressed[:] >> 3 & 1
+    uncompressed[5::8] = compressed[:] >> 2 & 1
+    uncompressed[6::8] = compressed[:] >> 1 & 1
+    uncompressed[7::8] = compressed[:] & 1
 
-  return uncompressed
+    return uncompressed
 
 
 SPLIT_SEQUENCES = {
@@ -35,65 +35,65 @@ VOXEL_DIMS = (256, 256, 32)
 
 
 class SSCDataset:
-  def __init__(self, directory, split="train"):
-    """ Load data from given dataset directory. """
+    def __init__(self, directory, split="train"):
+        """ Load data from given dataset directory. """
 
-    self.files = {}
-    self.filenames = []
+        self.files = {}
+        self.filenames = []
 
-    for ext in SPLIT_FILES[split]:
-      self.files[EXT_TO_NAME[ext]] = []
+        for ext in SPLIT_FILES[split]:
+            self.files[EXT_TO_NAME[ext]] = []
 
-    for sequence in SPLIT_SEQUENCES[split]:
-      complete_path = os.path.join(directory, "sequences", sequence, "voxels")
-      if not os.path.exists(complete_path): raise RuntimeError("Voxel directory missing: " + complete_path)
+        for sequence in SPLIT_SEQUENCES[split]:
+            complete_path = os.path.join(directory, "sequences", sequence, "voxels")
+            if not os.path.exists(complete_path): raise RuntimeError("Voxel directory missing: " + complete_path)
 
-      files = os.listdir(complete_path)
-      for ext in SPLIT_FILES[split]:
-        data = sorted([os.path.join(complete_path, f) for f in files if f.endswith(ext)])
-        if len(data) == 0: raise RuntimeError("Missing data for " + EXT_TO_NAME[ext])
-        self.files[EXT_TO_NAME[ext]].extend(data)
+            files = os.listdir(complete_path)
+            for ext in SPLIT_FILES[split]:
+                data = sorted([os.path.join(complete_path, f) for f in files if f.endswith(ext)])
+                if len(data) == 0: raise RuntimeError("Missing data for " + EXT_TO_NAME[ext])
+                self.files[EXT_TO_NAME[ext]].extend(data)
 
-      # this information is handy for saving the data later, since you need to provide sequences/XX/predictions/000000.label:
-      self.filenames.extend(
-          sorted([(sequence, os.path.splitext(f)[0]) for f in files if f.endswith(SPLIT_FILES[split][0])]))
+            # this information is handy for saving the data later, since you need to provide sequences/XX/predictions/000000.label:
+            self.filenames.extend(
+                sorted([(sequence, os.path.splitext(f)[0]) for f in files if f.endswith(SPLIT_FILES[split][0])]))
 
-    self.num_files = len(self.filenames)
+        self.num_files = len(self.filenames)
 
-    # sanity check:
-    for k, v in self.files.items():
-      print(k, len(v))
-      assert (len(v) == self.num_files)
+        # sanity check:
+        for k, v in self.files.items():
+            print(k, len(v))
+            assert (len(v) == self.num_files)
 
-  def __len__(self):
-    return self.num_files
+    def __len__(self):
+        return self.num_files
 
-  def __getitem__(self, t):
-    """ fill dictionary with available data for given index . """
-    collection = {}
+    def __getitem__(self, t):
+        """ fill dictionary with available data for given index . """
+        collection = {}
 
-    # read raw data and unpack (if necessary)
-    for typ in self.files.keys():
-      scan_data = None
-      if typ == "label":
-        scan_data = np.fromfile(self.files[typ][t], dtype=np.uint16)
-      else:
-        scan_data = unpack(np.fromfile(self.files[typ][t], dtype=np.uint8))
+        # read raw data and unpack (if necessary)
+        for typ in self.files.keys():
+            scan_data = None
+            if typ == "label":
+                scan_data = np.fromfile(self.files[typ][t], dtype=np.uint16)
+            else:
+                scan_data = unpack(np.fromfile(self.files[typ][t], dtype=np.uint8))
 
-      # turn in actual voxel grid representation.
-      collection[typ] = scan_data.reshape(VOXEL_DIMS)
+            # turn in actual voxel grid representation.
+            collection[typ] = scan_data.reshape(VOXEL_DIMS)
 
-    return self.filenames[t], collection
+        return self.filenames[t], collection
 
 
 if __name__ == "__main__":
-  # Small example of the usage.
-  
-  # Replace "/path/to/semantic/kitti/" with actual path to the folder containing the "sequences" folder
-  dataset = SSCDataset("/path/to/semantic/kitti/")
-  print("# files: {}".format(len(dataset)))
+    # Small example of the usage.
 
-  (seq, filename), data = dataset[100]
-  print("Contents of entry 100 (" + seq + ":" + filename + "):")
-  for k, v in data.items():
-    print("  {:8} : shape = {}, contents = {}".format(k, v.shape, np.unique(v)))
+    # Replace "/path/to/semantic/kitti/" with actual path to the folder containing the "sequences" folder
+    dataset = SSCDataset("/path/to/semantic/kitti/")
+    print("# files: {}".format(len(dataset)))
+
+    (seq, filename), data = dataset[100]
+    print("Contents of entry 100 (" + seq + ":" + filename + "):")
+    for k, v in data.items():
+        print("  {:8} : shape = {}, contents = {}".format(k, v.shape, np.unique(v)))
--- content.py
+++ content.py
@@ -8,129 +8,128 @@ import numpy as np
 import collections
 from auxiliary.laserscan import SemLaserScan
 
-
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser("./content.py")
-  parser.add_argument(
-      '--dataset', '-d',
-      type=str,
-      required=True,
-      help='Dataset to calculate content. No Default',
-  )
-  parser.add_argument(
-      '--config', '-c',
-      type=str,
-      required=False,
-      default="config/semantic-kitti.yaml",
-      help='Dataset config file. Defaults to %(default)s',
-  )
-  FLAGS, unparsed = parser.parse_known_args()
+    parser = argparse.ArgumentParser("./content.py")
+    parser.add_argument(
+        '--dataset', '-d',
+        type=str,
+        required=True,
+        help='Dataset to calculate content. No Default',
+    )
+    parser.add_argument(
+        '--config', '-c',
+        type=str,
+        required=False,
+        default="config/semantic-kitti.yaml",
+        help='Dataset config file. Defaults to %(default)s',
+    )
+    FLAGS, unparsed = parser.parse_known_args()
 
-  # print summary of what we will do
-  print("*" * 80)
-  print("INTERFACE:")
-  print("Dataset", FLAGS.dataset)
-  print("Config", FLAGS.config)
-  print("*" * 80)
-
-  # open config file
-  try:
-    print("Opening config file %s" % FLAGS.config)
-    CFG = yaml.safe_load(open(FLAGS.config, 'r'))
-  except Exception as e:
-    print(e)
-    print("Error opening yaml file.")
-    quit()
-
-  # get training sequences to calculate statistics
-  sequences = CFG["split"]["train"]
-  print("Analizing sequences", sequences)
-
-  # create content accumulator
-  accum = {}
-  total = 0.0
-  for key, _ in CFG["labels"].items():
-    accum[key] = 0
-
-  # itearate over sequences
-  for seq in sequences:
-    seq_accum = {}
-    seq_total = 0.0
-    for key, _ in CFG["labels"].items():
-      seq_accum[key] = 0
-
-    # make seq string
+    # print summary of what we will do
     print("*" * 80)
-    seqstr = '{0:02d}'.format(int(seq))
-    print("parsing seq {}".format(seq))
+    print("INTERFACE:")
+    print("Dataset", FLAGS.dataset)
+    print("Config", FLAGS.config)
+    print("*" * 80)
 
-    # does sequence folder exist?
-    scan_paths = os.path.join(FLAGS.dataset, "sequences",
-                              seqstr, "velodyne")
-    if os.path.isdir(scan_paths):
-      print("Sequence folder exists!")
-    else:
-      print("Sequence folder doesn't exist! Exiting...")
-      quit()
+    # open config file
+    try:
+        print("Opening config file %s" % FLAGS.config)
+        CFG = yaml.safe_load(open(FLAGS.config, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening yaml file.")
+        quit()
 
-    # populate the pointclouds
-    scan_names = [os.path.join(dp, f) for dp, dn, fn in os.walk(
-        os.path.expanduser(scan_paths)) for f in fn]
-    scan_names.sort()
+    # get training sequences to calculate statistics
+    sequences = CFG["split"]["train"]
+    print("Analizing sequences", sequences)
 
-    # does sequence folder exist?
-    label_paths = os.path.join(FLAGS.dataset, "sequences",
-                               seqstr, "labels")
-    if os.path.isdir(label_paths):
-      print("Labels folder exists!")
-    else:
-      print("Labels folder doesn't exist! Exiting...")
-      quit()
-    # populate the pointclouds
-    label_names = [os.path.join(dp, f) for dp, dn, fn in os.walk(
-        os.path.expanduser(label_paths)) for f in fn]
-    label_names.sort()
+    # create content accumulator
+    accum = {}
+    total = 0.0
+    for key, _ in CFG["labels"].items():
+        accum[key] = 0
 
-    # check that there are same amount of labels and scans
-    print(len(label_names))
-    print(len(scan_names))
-    assert(len(label_names) == len(scan_names))
+    # itearate over sequences
+    for seq in sequences:
+        seq_accum = {}
+        seq_total = 0.0
+        for key, _ in CFG["labels"].items():
+            seq_accum[key] = 0
 
-    # create a scan
-    nclasses = len(CFG["labels"])
-    scan = SemLaserScan(nclasses, CFG["color_map"], project=False)
+        # make seq string
+        print("*" * 80)
+        seqstr = '{0:02d}'.format(int(seq))
+        print("parsing seq {}".format(seq))
 
-    for idx in range(len(scan_names)):
-      # open scan
-      print(label_names[idx])
-      scan.open_scan(scan_names[idx])
-      scan.open_label(label_names[idx])
-      # make histogram and accumulate
-      count = np.bincount(scan.sem_label)
-      seq_total += count.sum()
-      for key, data in seq_accum.items():
-        if count.size > key:
-          seq_accum[key] += count[key]
-          # zero the count
-          count[key] = 0
-      for i, c in enumerate(count):
-        if c > 0:
-          print("wrong label ", i, ", nr: ", c)
+        # does sequence folder exist?
+        scan_paths = os.path.join(FLAGS.dataset, "sequences",
+                                  seqstr, "velodyne")
+        if os.path.isdir(scan_paths):
+            print("Sequence folder exists!")
+        else:
+            print("Sequence folder doesn't exist! Exiting...")
+            quit()
 
-    seq_accum = collections.OrderedDict(
-        sorted(seq_accum.items(), key=lambda t: t[0]))
+        # populate the pointclouds
+        scan_names = [os.path.join(dp, f) for dp, dn, fn in os.walk(
+            os.path.expanduser(scan_paths)) for f in fn]
+        scan_names.sort()
 
-    # print and send to total
-    total += seq_total
-    print("seq ", seqstr, "total", seq_total)
-    for key, data in seq_accum.items():
-      accum[key] += data
-      print(data)
+        # does sequence folder exist?
+        label_paths = os.path.join(FLAGS.dataset, "sequences",
+                                   seqstr, "labels")
+        if os.path.isdir(label_paths):
+            print("Labels folder exists!")
+        else:
+            print("Labels folder doesn't exist! Exiting...")
+            quit()
+        # populate the pointclouds
+        label_names = [os.path.join(dp, f) for dp, dn, fn in os.walk(
+            os.path.expanduser(label_paths)) for f in fn]
+        label_names.sort()
 
-  # print content to fill yaml file
-  print("*" * 80)
-  print("Content in training set")
-  print(accum)
-  accum = collections.OrderedDict(sorted(accum.items(), key=lambda t: t[0]))
-  for key, data in accum.items():
-    print(" {}: {}".format(key, data / total))
+        # check that there are same amount of labels and scans
+        print(len(label_names))
+        print(len(scan_names))
+        assert (len(label_names) == len(scan_names))
+
+        # create a scan
+        nclasses = len(CFG["labels"])
+        scan = SemLaserScan(nclasses, CFG["color_map"], project=False)
+
+        for idx in range(len(scan_names)):
+            # open scan
+            print(label_names[idx])
+            scan.open_scan(scan_names[idx])
+            scan.open_label(label_names[idx])
+            # make histogram and accumulate
+            count = np.bincount(scan.sem_label)
+            seq_total += count.sum()
+            for key, data in seq_accum.items():
+                if count.size > key:
+                    seq_accum[key] += count[key]
+                    # zero the count
+                    count[key] = 0
+            for i, c in enumerate(count):
+                if c > 0:
+                    print("wrong label ", i, ", nr: ", c)
+
+        seq_accum = collections.OrderedDict(
+            sorted(seq_accum.items(), key=lambda t: t[0]))
+
+        # print and send to total
+        total += seq_total
+        print("seq ", seqstr, "total", seq_total)
+        for key, data in seq_accum.items():
+            accum[key] += data
+            print(data)
+
+    # print content to fill yaml file
+    print("*" * 80)
+    print("Content in training set")
+    print(accum)
+    accum = collections.OrderedDict(sorted(accum.items(), key=lambda t: t[0]))
+    for key, data in accum.items():
+        print(" {}: {}".format(key, data / total))
